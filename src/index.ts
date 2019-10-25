f469ae7eff1e35e8d3d615cf002cc2b9cd27b506
ParkSungHo007 <gr347034@graded.br>
2019-10-24 22:05:22 -0300

    still editing

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,9 @@
 import { Ball } from "./modules/ball.js";
 import { Bubble } from "./modules/bubble.js";
 import { Snowflake } from "./modules/snowflakes.js";
-
+// tslint:disable-next-line: no-var-requires
+let Calculess = require("calculess");
+let Calc = Calculess.prototype;
 let balls: Ball[] = [];
 let snowflakes: Snowflake[] = [];
 let bubbles: Bubble[] = [];
@@ -38,39 +40,41 @@ function setup() {
     for (let i = 0; i < numFlakes; i++) {
         snowflakes[i] = new Snowflake(random(25, width - 25), random(25, height - 25), random(10, 50));
     }
-    function getCol(): string {
-        // color id is 6 letters with # in front. There are 16 letters possible for all the different possibilites
-        // 0 ~ 9 and A~ F
-        let letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-        let colSymbol = "#";
-        for (let i = 0; i < 6; i++) {
-            colSymbol = colSymbol + letters[Math.floor(Math.random() * 16)];
-        }
-        return colSymbol;
-    }
     /* TODO OPTIONAL - add a function mousePressed() that either stops or starts objects from moving
        if the mouse is pressed while it is touching them. So you could use this (if careful!) to stop all of the
        objects from moving then start them back up again. The Ball class has some helper functions that will
        help you with this, but you'll need to add them to the other classes.
     */
-    function draw() {
-        background("skyblue");
-        for (let i = 0; i < balls.length; i++) {
-            balls[i].draw();
-            balls[i].move();
-        }
-        for (let c = 0; c < bubbles.length; c++) {
-            bubbles[c].draw();
-            bubbles[c].move();
-        }
-        for (let s = 0; s < snowflakes.length; s++) {
-            snowflakes[s].draw();
-            snowflakes[s].move();
-        }
-        // do not edit the below lines
-        window.draw = draw;
-        window.setup = setup;
-        window.mousePressed = mousePressed;
-        window.mouseReleased = mouseReleased;
+
+    // do not edit the below lines
+}
+function getCol(): string {
+    // color id is 6 letters with # in front. There are 16 letters possible for all the different possibilites
+    // 0 ~ 9 and A~ F
+    let letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+    let colSymbol = "#";
+    for (let i = 0; i < 6; i++) {
+        colSymbol = colSymbol + letters[Math.floor(Math.random() * 16)];
     }
+    return colSymbol;
+}
+function draw() {
+    background("skyblue");
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].move();
+    }
+    for (let c = 0; c < bubbles.length; c++) {
+        bubbles[c].draw();
+        bubbles[c].move();
+    }
+    for (let s = 0; s < snowflakes.length; s++) {
+        snowflakes[s].draw();
+        snowflakes[s].move();
+    }
+
+    window.draw = draw;
+    window.setup = setup;
+    window.mousePressed = mousePressed;
+    window.mouseReleased = mouseReleased;
 }
